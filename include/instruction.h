f98928adcc0fb84d6f0236ea9f4fd6633d61838a
Graeme Morris <vmaxson.dev@gmail.com>
2025-03-22 17:15:20 -0600

    feat: add raw  instruction

--- include/instruction.h
+++ include/instruction.h
@@ -4,11 +4,12 @@
 #include <stdint.h>
 
 typedef struct yac_instruction {
-    uint8_t opcode;
-    uint8_t x;
-    uint8_t y;
-    uint8_t kk;
-    uint16_t nnn;
+	uint16_t raw;
+	uint8_t opcode;
+	uint8_t x;
+	uint8_t y;
+	uint8_t kk;
+	uint16_t nnn;
 } yac_instruction;
 
 /// Decodes a CHIP-8 instruction given the raw instruction bytes.
--- src/instruction.c
+++ src/instruction.c
@@ -3,6 +3,7 @@
 yac_instruction yac_decode_instruction(const uint8_t msb, const uint8_t lsb)
 {
 	yac_instruction instr;
+	instr.raw = (msb << 8) | lsb;
 	instr.opcode = (msb & 0xF0) >> 4;
 	instr.x = msb & 0x0F;
 	instr.y = (lsb & 0xF0) >> 4;
