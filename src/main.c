0665cf3e5a571b8a2bcb26102a79c5f585ba35cc
maxson <vmaxson.dev@gmail.com>
2025-03-22 00:14:32 -0600

    feat: utilize p layer

--- src/main.c
+++ src/main.c
@@ -1,24 +1,44 @@
 #include <stdlib.h>
 #include "cpu.h"
+#include "platform.h"
 
 int main(void)
 {
+	const int display_width = 64;
+	const int display_height = 32;
+
 	const yac_cpu_config config = {
 		.memory_size = 4096,
 		.registers_size = 16,
-		.display_width = 64,
-		.display_height = 32
+		.display_width = display_width,
+		.display_height = display_height,
 	};
 
 	yac_cpu *cpu = yac_cpu_new(config);
+
+	const yac_platform_config platform_config = {
+		.window_title = "Yet Another CHIP-8 Emulator",
+		.window_width = display_width * 10,
+		.window_height = display_height * 10,
+	};
+
+	yac_platform platform_layer = yac_platform_new(platform_config);
 
 	while (1) {
 		if (!yac_cpu_cycle(cpu)) {
 			break;
 		}
+
+		if (cpu->redraw_requested) {
+			yac_platform_render(&platform_layer,
+					    &cpu->display_memory, display_width,
+					    display_height, 0xFFFFFFFF);
+			cpu->redraw_requested = false;
+		}
 	}
 
 	yac_cpu_del(cpu);
+	yac_platform_del(&platform_layer);
 
 	return EXIT_SUCCESS;
 }
