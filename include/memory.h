c5a9e0185bf01b6c73534d710b8cdbc1508be0f5
Graeme Morris <vmaxson.dev@gmail.com>
2025-03-22 17:17:09 -0600

    style: remove extra indents

--- include/memory.h
+++ include/memory.h
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define DEFINE_YAC_MEMORY_TYPE(type)								\
-typedef struct yac_memory_##type {								\
-	size_t size;										\
-	type *data;										\
-} yac_memory_##type;										\
-yac_memory_##type yac_memory_##type##_new(size_t size);						\
-void yac_memory_##type##_del(yac_memory_##type *mem);						\
+#define DEFINE_YAC_MEMORY_TYPE(type)						\
+typedef struct yac_memory_##type {						\
+	size_t size;								\
+	type *data;								\
+} yac_memory_##type;								\
+yac_memory_##type yac_memory_##type##_new(size_t size);				\
+void yac_memory_##type##_del(yac_memory_##type *mem);				\
 
 typedef uint8_t u8;
 
--- src/memory.c
+++ src/memory.c
@@ -1,14 +1,14 @@
 #include "memory.h"
 
-#define DEFINE_YAC_MEMORY_TYPE_IMPL(type)							\
-yac_memory_##type yac_memory_##type##_new(size_t size) {					\
-	yac_memory_##type mem;									\
-	mem.size = size;									\
-	mem.data = (type *)malloc(size * sizeof(type));						\
-	return mem;										\
-}												\
-void yac_memory_##type##_del(yac_memory_##type *mem) {						\
-	free(mem->data);									\
+#define DEFINE_YAC_MEMORY_TYPE_IMPL(type)					\
+yac_memory_##type yac_memory_##type##_new(size_t size) {			\
+	yac_memory_##type mem;							\
+	mem.size = size;							\
+	mem.data = (type *)malloc(size * sizeof(type));				\
+	return mem;								\
+}										\
+void yac_memory_##type##_del(yac_memory_##type *mem) {				\
+	free(mem->data);							\
 }
 
 DEFINE_YAC_MEMORY_TYPE_IMPL(u8)
